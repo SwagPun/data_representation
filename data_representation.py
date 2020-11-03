30224490cc27967b6105a486d2b6658efb926f32
Mr Scott <mscott@brightoncollege.net>
2020-11-03 14:32:35 +0000

    Completed base classes

--- data_representation.py
+++ data_representation.py
@@ -38,16 +38,8 @@ class FloatingPointNumber(BinaryString):
 
 
 if __name__ == '__main__':
-    x = FloatingPointNumber("010101")
-    print(x)
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    x = FloatingPointNumber("010101", "001")
+    y = FloatingPointNumber.as_single_string("010101001", 6)
+    print(x, y)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/

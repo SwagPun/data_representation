90c9fa8cbd0c5d779e0e881eb93a5d02c20b73f4
Mr Scott <mscott@brightoncollege.net>
2020-11-04 16:59:21 +0000

    Added additional classes and magic functions to represent fixedpoint and twos complement

--- data_representation.py
+++ data_representation.py
@@ -9,7 +9,7 @@ class BinaryString:
 
     @property
     def value(self):
-        return self.__value
+        return self._value
 
     @value.setter
     def value(self, value):
@@ -18,10 +18,69 @@ class BinaryString:
         for digit in value:
             if digit not in ["1", "0"]:
                 raise ValueError("Binary string given contains values that are not 1 or 0")
-        self.__value = value
+        self._value = value
 
     def __str__(self):
         return self.value
+
+    def __add__(self, other):
+        return BinaryString(str(self) + str(other))
+
+    def __len__(self):
+        return len(self.value)
+
+    def __invert__(self):
+        inverted = ""
+        for bit in self.value:
+            if bit == "1":
+                inverted += "0"
+            else:
+                inverted += "1"
+        return inverted
+
+
+class FixPointNumber(BinaryString):
+    def __init__(self, binary, point):
+        if type(point) != int:
+            raise ValueError("The argument <point> must be a whole number")
+        self._binary_point = point
+        super(FixPointNumber, self).__init__(binary)
+
+    def __float__(self):
+        column_value = 2 ** (len(self)-1)
+        total = 0
+        for bit in self.value:
+            total += int(bit) * column_value
+            column_value /= 2
+        total = total * (2**(self._binary_point-len(self)))
+        return total
+
+    def __int__(self):
+        return int(float(self))
+
+    def __str__(self):
+        if self._binary_point < len(self):
+            return self.value[:self._binary_point] + "." + self.value[self._binary_point:]
+        else:
+            return self.value
+
+
+class TwosComplementNumber(BinaryString):
+    def __abs__(self):
+        """ Returns whole number part of twos complement number as BinaryString"""
+        pass
+
+    def to_positive(self):
+        pass
+
+    def __neg__(self):
+        pass
+
+    def is_positive(self):
+        pass
+
+    def is_negative(self):
+        pass
 
 
 class FloatingPointNumber():
@@ -33,13 +92,40 @@ class FloatingPointNumber():
     def as_single_string(cls, binary, mantissa_size):
         return cls(binary[:mantissa_size], binary[mantissa_size:])
 
+    @property
+    def mantissa(self):
+        return self._mantissa
 
+    @mantissa.setter
+    def mantissa(self, mantissa):
+        self._mantissa = mantissa
 
+    @property
+    def exponent(self):
+        return self._exponent
+
+    @exponent.setter
+    def exponent(self, exponent):
+        self._exponent = exponent
+
+    def __str__(self):
+        return str(self.mantissa + self.exponent)
+
+    def __int__(self):
+
+        #Convert exponent from twos complement
+
+        #Convert to integer
+
+        #Convert mantissa from twos complement
+
+        #Convert to integer
+        pass
 
 
 if __name__ == '__main__':
-    x = FloatingPointNumber("010101", "001")
-    y = FloatingPointNumber.as_single_string("010101001", 6)
-    print(x, y)
+    x = FloatingPointNumber("011", 3)
+    print(x)
+    print(~x)
 
 
